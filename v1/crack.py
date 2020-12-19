bb5a44349555d42e2704dabd76e46f85a60ab210
1MiKHalyCH1 <bykoun@gmail.com>
2020-12-19 16:31:31 +0500

    v1

--- v1/crack.py
+++ v1/crack.py
@@ -16,21 +16,6 @@ def split(x):
         if x ^ v in U:
             return v, x ^ v
 
-def encrypt(x, y, key):
-    keys = [key]
-    for _ in range(ROUNDS-1):
-        keys.append(K[keys[-1]])
-    print(keys)
-
-    for i in keys:
-        print(f'x: {split(x)[1]}, y: {split(y)[1]}, i: {split(i)[1]}')
-        print(f'{split(y)[1]}', f'{split(x)[1]} ^ {split(S[y])[1]} ^ {split(i)[1]} = {split(x^S[y]^i)[1]}')
-        print()
-        x, y = y, x^S[y]^i
-    print(split(x), split(y))
-
-    return x, y
-
 def check_key(pt, ct):
     x,     y     = pt >> 6, pt & ((1<<6)-1)
     res_x, res_y = ct >> 6, ct & ((1<<6)-1)
@@ -48,20 +33,20 @@ def crack_key(cipher):
         cosets[split(x)[1]].add(x)
 
     right_cosets = set()
-    for x in range(1<<6):
-        pt = (x << 6) | S[x]
+    pts = [((x << 6) | S[x]) for x in range(1<<6)]
+    for pt in pts:
         ct = cipher.encrypt(pt)
         if check_key(pt, ct):
-            right_cosets.add(split(x)[1])
+            right_cosets.add(split(pt >> 6)[1])
 
     for coset in right_cosets:
         for key in cosets[coset]:
-            if all(Cipher(key, ROUNDS, 12).decrypt(cipher.encrypt((x<<6) | S[x])) == ((x<<6) | S[x]) for x in S):
+            if all(Cipher(key, ROUNDS, 12).decrypt(cipher.encrypt(pt)) == pt for pt in pts):
                 return key
 
 
 if __name__ == '__main__':
-    for i in range(1<<6):
+    for key in range(1<<6):
         cipher = Cipher(i, ROUNDS, 12)
         assert crack_key(cipher) == i
     print('Done!')
