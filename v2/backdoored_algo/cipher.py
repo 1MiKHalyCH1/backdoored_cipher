33984521df8af7d0714968bb01c5ba3f3faf1298
1MiKHalyCH1 <bykoun@gmail.com>
2020-12-19 18:40:11 +0500

    v2

---
+++ v2/backdoored_algo/cipher.py
@@ -0,0 +1,40 @@
+from backdoored_algo.const import S, K
+from backdoored_algo.utils import int_to_blocks, blocks_to_int, invert_S
+from itertools import cycle
+
+S_inv = invert_S(S)
+
+class Cipher:
+    def __init__(self, key, rounds, block_size):
+        self._rounds = rounds
+        self._keys = self._expand_key(key)
+        self._block_size = block_size
+        self._half_block_size = block_size >> 1
+
+    def _expand_key(self, key):
+        keys = [key]
+        for _  in range(self._rounds-1):
+            keys.append(K[keys[-1]])
+        return keys
+
+    def _encrypt_block(self, block):
+        L, R = block >> self._half_block_size, block & ((1 << self._half_block_size) - 1)
+        for i in range(self._rounds):
+            L, R = R, L ^ (S[R^ self._keys[i]])
+        return (L << self._half_block_size) | R
+
+    def _decrypt_block(self, block):
+        L, R = block >> self._half_block_size, block & ((1 << self._half_block_size) - 1)
+        for i in range(self._rounds):
+            R, L = L, R ^ (S[L ^ self._keys[self._rounds - i - 1]])
+        return (L << self._half_block_size) | R
+
+    def encrypt(self, pt):
+        pt_blocks = int_to_blocks(pt, self._block_size)
+        ct_blocks = [self._encrypt_block(block) for block in pt_blocks]
+        return blocks_to_int(ct_blocks, self._block_size)
+
+    def decrypt(self, ct):
+        ct_blocks = int_to_blocks(ct, self._block_size)
+        pt_blocks = [self._decrypt_block(block) for block in ct_blocks]
+        return blocks_to_int(pt_blocks, self._block_size)
---
+++ v2/crack.py
@@ -0,0 +1,60 @@
+from random import choice
+
+from backdoored_algo.cipher import Cipher
+from backdoored_algo.const import S, K
+from secret import V, U, ro, teta
+
+ROUNDS = 5
+
+ro_inv = {ro[x]:x for x in ro}
+PTS = list(range(1 << 12))
+
+def split(x):
+    for v in V:
+        if x ^ v in U:
+            return v, x ^ v
+
+def check_key(pt, ct):
+    x,     y     = pt >> 6, pt & ((1<<6)-1)
+    res_x, res_y = ct >> 6, ct & ((1<<6)-1)
+
+    cosets = [split(x)[1]]
+    for _ in range(ROUNDS):
+        cosets.append(ro[cosets[-1]])
+
+    if cosets[-1] == split(res_x)[1]:
+        return True
+
+def crack_key(cipher, cosets, keys):
+    for u in U:
+        k = keys[u]
+        k0_inv, k1_inv = ro_inv[0]^k[0], ro_inv[0]^k[1]
+        x = choice(list(cosets[k1_inv]))
+        y = choice(list(cosets[k0_inv]))
+        pt = (x << 6) | y
+        ct = cipher.encrypt(pt)
+
+        res_x, res_y = split(ct >> 6)[1], split(ct & ((1<<6)-1))[1]
+
+        if ro_inv[res_x ^ k0_inv] ^ k[3] == ro[res_x ^ k[4]] ^ res_y:
+            for key in cosets[u]:
+                if all(Cipher(key, ROUNDS, 12).decrypt(cipher.encrypt(pt)) == pt for pt in PTS):
+                    return key
+
+
+if __name__ == '__main__':
+    cosets = {x:set() for x in U}
+    for x in range(len(S)):
+        cosets[split(x)[1]].add(x)
+
+    keys = {}
+    for u in U:
+        k = [u]
+        for _ in range(ROUNDS):
+            k.append(ro[k[-1]])
+        keys[u] = k
+
+    for key in range(1<<6):
+        cipher = Cipher(key, ROUNDS, 12)
+        assert crack_key(cipher, cosets, keys) == key
+    print('Done!')
